1cb733ada6ce3958a86d470021091556ed4e09a1
Sgian24 <93100806+Sgian24@users.noreply.github.com>
2025-12-14 22:03:06 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Component, useState } from 'react'
+import './App.css'
+import Com from './component.jsx'
+
+const App = () => {
+  
+  return (
+    <>
+      <Com />
+      <div>
+        <p>Wifey is a gooba</p>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/component.jsx
@@ -0,0 +1,9 @@
+const Com = () => {
+    return (
+        <>
+        <p>NavBar</p>
+        </>
+    )
+}
+
+export default Com;
